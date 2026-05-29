96331a6eddbd66b02026e025f46eee99320a5742
Pratyush Jena <prattjena69@gmail.com>
2026-05-29 10:23:29 -0700

    add: .env.example for example structure of env variables

--- graph/utils/llm.py
+++ graph/utils/llm.py
@@ -1,11 +1,8 @@
 import os
 from langchain.chat_models import init_chat_model
 
-# model_name = os.getenv("LLM_MODEL", "gpt-4o")
-# model_provider = os.getenv("LLM_PROVIDER", "openai")
-
-model_name = "gpt-4o"
-model_provider = "openai"
+model_name = os.getenv("LLM_MODEL")
+model_provider = os.getenv("LLM_PROVIDER")
 
 llm = init_chat_model(
     model=model_name,
