29db82eab5b63b93d0eba4f57b8f269e73a9e3eb
Pratyush Jena <prattjena69@gmail.com>
2026-05-29 07:58:09 -0700

    temp fix: defautling to gpt-4o

--- graph/utils/llm.py
+++ graph/utils/llm.py
@@ -1,8 +1,14 @@
 import os
 from langchain.chat_models import init_chat_model
 
+# model_name = os.getenv("LLM_MODEL", "gpt-4o")
+# model_provider = os.getenv("LLM_PROVIDER", "openai")
+
+model_name = "gpt-4o"
+model_provider = "openai"
+
 llm = init_chat_model(
-    model=os.getenv("LLM_MODEL", "gpt-4o"),
-    model_provider=os.getenv("LLM_PROVIDER", "openai"),
+    model=model_name,
+    model_provider=model_provider,
     temperature=0,
 )
